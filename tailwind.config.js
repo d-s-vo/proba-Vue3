e89516c4387fe88094b35a56ec813bd4f597d855
Dmtr Smirnov <d.smirnov.vo@mail.ru>
2023-09-29 19:48:27 +0300

    практика с всплывающими блоками и position:sticky

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {},
+  },
+  presets: [require("./node_modules/open-color/open-color")],
+  plugins: [],
+}
+
